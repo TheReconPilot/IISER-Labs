796519d0f4fc93cee798e258515fa5d43013b3ed
Purva <purvaparmar1405@gmail.com>
2020-01-13 20:52:19 +0530

    Finish Dynamic Method Calculations

--- PHY 222/Millikan's Oil Drop/Calculations - Dynamic Method.py
+++ PHY 222/Millikan's Oil Drop/Calculations - Dynamic Method.py
@@ -1,11 +1,16 @@
-import numpy as np 
-from uncertainties import ufloat
+import numpy as np
+from uncertainties import ufloat 
 from uncertainties import unumpy as unp 
+from scipy.optimize import curve_fit
 
-# Display List function to conveniently check lists
-def display(plist):
+# Display List function to conveniently print lists 
+# with a given number of decimals in an uncertainties object
+# and a specific format
+def display(plist, decimals, format_type="e"):
+    format_string = f"{{:.{decimals}{format_type}P}}"
     for i in range(len(plist)):
-        print(plist[i])
+        print(f"{format_string}".format(plist[i]))
+    print()     # Empty Line for formatting aesthetics
     return
 
 # Load Data
@@ -41,20 +46,8 @@ D = (9 * eta) / (2 * g * d_rho)
 sigma = c / (2 * P)
 
 # Calculate Mean Fall and Rise times
-tf = []     # Mean Free Fall Time
-tr = []     # Mean Rise Time
-
-# Length of fall_time and rise_time is same
-for i in range(len(fall_time)):
-    Sum_ft = 0
-    Sum_rt = 0
-    for j in range(len(fall_time[i])):
-        Sum_ft += fall_time[i][j]
-        Sum_rt += rise_time[i][j]
-
-    tf.append(Sum_ft/len(fall_time[i]))
-    tr.append(Sum_rt/len(fall_time[i]))
-
+tf = [np.mean(i) for i in fall_time]
+tr = [np.mean(i) for i in rise_time]
 
 # Mean Free Fall Velocity
 vf = [L / tf[i] for i in range(len(tf))]
@@ -103,12 +96,19 @@ for i in range(len(ne_by_min_ne)):
 
 # ne divided by nearest effective integer 
 ne_by_neff = []
+
+# Defining x and y lists for later use in linear regression
+x = []      # nearest_neff - Nearest Effective Integer, with 0.5 cases removed
+y = []      # new_ne - ne with cases corresponding to 0.5 closest integer removed
+
 for i in range(len(nearest_neff)):
     if(nearest_neff[i]%1 == 0):     # Reject the 0.5 cases
         ne_by_neff.append(new_ne[i] / nearest_neff[i])
+        x.append(nearest_neff[i].n)
+        y.append(new_ne[i].n)
 
-# Trying out all differences in ne and finding lowest
 
+# Trying out all differences in ne and finding lowest         
 lowest_diff_ne = abs(ne[1] - ne[0])
 for i in range(1, len(ne)):
     for j in range(i):
@@ -116,3 +116,89 @@ for i in range(1, len(ne)):
         if(diff < lowest_diff_ne):
             lowest_diff_ne = diff 
 
+# Define a linear function for regression
+def linear(x, m):
+    return m*x
+
+# Fitting the curve with scipy.optimize.curve_fit
+popt, pcov = curve_fit(linear, x, y) 
+
+# Final values of e
+
+## e as approximate gcd
+e = np.mean(ne_by_neff)
+
+## e from linear regression through points obtained above
+e_lr = ufloat(popt[0], np.sqrt(np.diag(pcov)))
+
+## Literature Value of e
+e_lv = 1.602E-19
+
+# Printing all the required stuff
+
+print("----------DYNAMIC METHOD----------")
+print("---Fall Times---")
+for i in range(len(fall_time)):
+    print(f"Droplet {i+1}")
+    display(fall_time[i], 1, "u")
+
+print("---Rise Times---")
+for i in range(len(rise_time)):
+    print(f"Droplet {i+1}")
+    display(rise_time[i], 1, "u")
+
+print("---Voltage---")
+display(V, 1, "u")    
+
+print("---Mean Fall Time---")
+display(tf, 1, "u")
+
+print("---Mean Rise Time---")
+display(tr, 1, "u")
+
+print("---Fall Velocity---")
+display(vf, 3)
+
+# Xi
+print("---\u03BE---")
+display(xi, 3)
+
+print("---r---")
+display(r, 3)
+
+# r^3
+print("---r\u00B3---")
+display(r3, 3)
+
+print("---T---")
+display(T, 1, "u")
+
+print("---ne---")
+display(ne, 3)
+
+print("---ne divided by lowest---")
+display(ne_by_min_ne, 1, "u")
+
+print("---Nearest integer neff---")
+for i in nearest_neff:
+    print(i.n)
+print()
+
+print("---ne/neff---")
+print("The 0.5 values are rejected automatically")
+display(ne_by_neff, 3)
+
+print("---Value of e---")
+print("Value of e as approximate gcd: {:.3eP}".format(e))
+print("Error = {:.2f}%\n".format((abs(e.n - e_lv) / e_lv) * 100))
+
+print("Value of e from regression: {:.3eP}".format(e_lr))
+print("Error = {:.2f}%\n".format((abs(e_lr.n - e_lv) / e_lv)*100))
+
+print("Lowest difference in values of ne: {:.3eP}".format(lowest_diff_ne))
+
+print()
+print("---Other Constants---")
+print("C = {:.3f}".format(C))
+print("D = {:.3e}".format(D))
+print("\u03C2 = {:.3e}".format(sigma))
